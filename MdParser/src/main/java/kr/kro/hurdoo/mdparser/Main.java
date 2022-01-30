921bfb889c27cedb0d853e696205aaf26a8b5b9f
64835865+HURDOO@users.noreply.github.com <64835865+HURDOO@users.noreply.github.com>
2022-01-30 22:59:58 +0900

    1.18.20.21

--- MdParser/src/main/java/kr/kro/hurdoo/mdparser/Main.java
+++ MdParser/src/main/java/kr/kro/hurdoo/mdparser/Main.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        KMF.parse(new File("1.18.10.28/1.18.10.28.md"));
+        KMF.parse(new File("1.18.20.21/1.18.20.21.md"));
     }
 
     public static String parseTable(String str) {
